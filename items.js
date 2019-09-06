cb6c3911a07405ab988fddb34fd61f5773262077
Nikaoto <nikaoto@gmail.com>
2019-09-07 00:06:42 +0400

    add key take func

---
+++ items.js
@@ -0,0 +1,8 @@
+// items.js
+// Defines items and their properties
+
+const ITEMS = {
+  TAKEABLE: {
+    ["K"]: {}
+  }
+}
--- main.js
+++ main.js
@@ -1,9 +1,12 @@
 // main.js
 // requires levels.js
+// requires items.js
 
 const CANVAS_WIDTH = 640
 const CANVAS_HEIGHT = 480
 const ACTION_DELAY = 120
+const CELL_WIDTH = 30
+const CELL_HEIGHT = 30
 
 const levelTitleHeader = document.querySelector("#level-title")
 
@@ -11,11 +14,8 @@ const editor = document.querySelector("#editor")
 const canvas = document.querySelector("#canvas")
 const ctx = canvas.getContext("2d")
 
-const cellWidth = 50,
-      cellHeight = 50
-
 const history = []
-let levelIndex = 0
+let levelIndex = 3
 
 // For student API
 Array.prototype.remove = function(i) {
@@ -26,12 +26,16 @@ Array.prototype.remove = function(i) {
 Array.prototype.insert = function(i, item) {
   this.splice(i, 0, item)
 }
-let evalEagerly = false
+
 
 let level = JSON.parse(JSON.stringify(LEVELS[levelIndex]))
 let levelWon = false
 let levelFinished = false // For student API
+
 const actionQueue = []
+let evalEagerly = false
+
+let heldItem = null
 
 function findLocation(objString, grid) {
   // Reference level if grid not passed (for student API)
@@ -52,30 +56,58 @@ function findLocation(objString, grid) {
 }
 
 function drawEmpty(x, y, offsetX = 0, offsetY = 0) {
-  ctx.clearRect(offsetX + cellWidth * x, offsetY + cellHeight * y, cellWidth, cellHeight)
+  ctx.clearRect(offsetX + CELL_WIDTH * x, offsetY + CELL_HEIGHT * y, CELL_WIDTH, CELL_HEIGHT)
   ctx.strokeStyle = "black"
   ctx.lineWidth = 2;
-  ctx.strokeRect(offsetX + cellWidth * x, offsetY + cellHeight * y, cellWidth, cellHeight)
+  ctx.strokeRect(offsetX + CELL_WIDTH * x, offsetY + CELL_HEIGHT * y, CELL_WIDTH, CELL_HEIGHT)
 }
 
 function drawHero(x, y, offsetX = 0, offsetY = 0) {
-  ctx.font = "40px Arial"
+  // Draw dim background
+  ctx.fillStyle = "rgba(255, 255, 255, 0.4)"
+  ctx.fillRect(offsetX + CELL_WIDTH * x + 5, offsetY + CELL_HEIGHT * y + 5, CELL_WIDTH - 10, CELL_HEIGHT - 10)
+  // Draw character
+  ctx.font = "25px Arial"
   ctx.fillStyle = "black"
-  ctx.fillText("@", offsetX + cellWidth * x + 5, offsetY + cellHeight * y + 36)
+  ctx.fillText("@", offsetX + CELL_WIDTH * x + 3, offsetY + CELL_HEIGHT * y + 22)
+  // Draw held item
+  const itemOffsetX = offsetX + Math.floor(CELL_WIDTH/3)
+  const itemOffsetY = offsetY + Math.floor(CELL_HEIGHT/3)
+  switch (heldItem) {
+  case null:
+    break;
+  case "@":
+    drawHero(x, y, itemOffsetX, itemOffsetY)
+    break;
+  case "E":
+    drawExit(x, y, itemOffsetX, itemOffsetY)
+    break;
+  case "K":
+    drawKey(x, y, itemOffsetX, itemOffsetY)
+    break;
+  default:
+    break;
+  }
 }
 
 function drawExit(x, y, offsetX = 0, offsetY = 0) {
-  ctx.font = "45px Arial"
+  ctx.font = "25px Arial"
   ctx.fillStyle = "black"
-  ctx.fillText("E", offsetX + cellWidth * x + 10, offsetY + cellHeight * y + 42)
+  ctx.fillText("E", offsetX + CELL_WIDTH * x + 5, offsetY + CELL_HEIGHT * y + 25)
+}
+
+function drawKey(x, y, offsetX = 0, offsetY = 0) {
+  ctx.font = "25px Arial"
+  ctx.fillStyle = "black"
+  ctx.fillText("K", offsetX + CELL_WIDTH * x + 5, offsetY + CELL_HEIGHT * y + 25)
 }
 
 function drawLevel(level) {
   // Get level total size
   const rowCount = level[0].length
   const colCount = level.length
-  const levelWidth = colCount * cellWidth
-  const levelHeight = rowCount * cellHeight
+  const levelWidth = colCount * CELL_WIDTH
+  const levelHeight = rowCount * CELL_HEIGHT
 
   // Set offsets to center level in canvas
   const offsetX = Math.floor((CANVAS_WIDTH - levelWidth) / 2)
@@ -86,17 +118,22 @@ function drawLevel(level) {
   for (let i = 0; i < level.length; i++) {
     for (let j = 0; j < level[i].length; j++) {
       level[i][j].forEach(obj => {
-        switch (obj) {
+        switch(obj) {
         case "":
           drawEmpty(i, j, offsetX, offsetY)
-          break
+          break;
         case "@":
           drawQueue.push(() => drawHero(i, j, offsetX, offsetY))
-          break
+          break;
         case "E":
           drawQueue.push(() => drawExit(i, j, offsetX, offsetY))
+          break;
+        case "K":
+          drawQueue.push(() => drawKey(i, j, offsetX, offsetY))
+          break;
         default:
           drawEmpty(i, j, offsetX, offsetY)
+          break;
         }
       })
     }
@@ -104,8 +141,8 @@ function drawLevel(level) {
 
   // Draw foreground items (from queue)
   while (drawQueue.length > 0) {
-    drawQueue[drawQueue.length-1]()
-    drawQueue.pop()
+    drawQueue[0]()
+    drawQueue.splice(0, 1)
   }
 }
 
@@ -118,10 +155,23 @@ function update() {
   drawLevel(level)
 }
 
-// Player functions
+function runAllActions() {
+  if (actionQueue[0]) {
+    // Perform first action in queue
+    actionQueue[0](level)
+
+    // Check collisions, conditions, and interacitons #TODO
+
+    // Remove performed action from queue
+    actionQueue.splice(0, 1)
+    // Run next action after delay
+    window.setTimeout(runAllActions, ACTION_DELAY)
+  }
+}
+
 function step(dir) {
   if (dir === null || dir === undefined) {
-    console.error("ERROR: invalid direction")
+    console.error(`WARNING: invalid direction "${dir}"`)
     return;
   }
 
@@ -145,12 +195,12 @@ function step(dir) {
     dy = 1
     break
   default:
-    console.error("ERROR: invalid direction")
+    console.error(`WARNING: invalid direction "${dir}"`)
     return;
   }
 
   // Push action onto FIFO queue
-  actionQueue.push((level) => {
+  actionQueue.push(level => {
     // Find player location
     const loc = findLocation("@", level)
     if (loc === null) {
@@ -167,7 +217,7 @@ function step(dir) {
     const colCount = level.length
     if (newX < 0 || newX > colCount - 1 ||
         newY < 0 || newY > rowCount - 1) {
-      console.error(`ERROR: Can not step("${dir}"), already at edge`)
+      console.error(`WARNING: Can not step("${dir}"), already at edge`)
       update()
       return;
     }
@@ -194,18 +244,40 @@ function step(dir) {
   }
 }
 
-function runAllActions() {
-  if (actionQueue[0]) {
-    // Perform first action in queue
-    actionQueue[0](level)
+function take() {
+  // Push action onto FIFO queue
+  actionQueue.push(level => {
+    // Find player location
+    const loc = findLocation("@", level)
+    if (loc === null) {
+      console.error("ERROR: player does not exist. You hacked my game!")
+      return;
+    }
 
-    // Check collisions, conditions, and interacitons #TODO
+    // Check if items can be taken
+    const takeableItems = Object.keys(ITEMS.TAKEABLE)
+    let nothingToTake = true
+    for (let i = 0; i < level[loc.x][loc.y].length; i++) {
+      if (takeableItems.includes(level[loc.x][loc.y][i])) {
+        nothingToTake = false
+        // Hold item
+        heldItem = level[loc.x][loc.y][i]
+        // Remove item from level
+        level[loc.x][loc.y].splice(i, 1)
+        update()
+        break;
+      }
+    }
 
-    // Remove performed action from queue
-    actionQueue.splice(0, 1)
-    // Run next action after delay
-    window.setTimeout(runAllActions, ACTION_DELAY)
-  }
+    if (nothingToTake) {
+      console.error("WARNING: Nothing to take")
+      return;
+    }
+  })
+}
+
+function put() {
+  
 }
 
 // Run button
@@ -255,6 +327,7 @@ nextButton.addEventListener("click", () => {
   nextButton.disabled = true
   levelWon = false
   levelFinished = false
+  heldItem = null
   update()
 
   // Update level text
@@ -268,6 +341,7 @@ resetButton.addEventListener("click", () => {
   nextButton.disabled = true
   levelWon = false
   levelFinished = false
+  heldItem = null
   level = JSON.parse(JSON.stringify(LEVELS[levelIndex]))
   update()
 })
